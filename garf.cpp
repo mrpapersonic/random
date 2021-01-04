934abbb1e02d7d0458bd43ce1c2de1fab8157087
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-01-04 13:52:42 -0500

    Add files via upload

---
+++ garf.cpp
@@ -0,0 +1,16 @@
+// garf simulator
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+char answer[100];
+std::cout << "would you like to monetize 1123.best?: ";
+gets(answer);
+if(answer=="yes"){
+std::cout << "it's a yes!";
+}else{
+std::cout << "it's a yes!";
+}
+return 0;
+}
---
+++ python3path.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Downloading pathed...
+if not exist %temp%\python3path mkdir %temp%\python3path
+powershell "iwr -OutFile "%temp%\python3path\pathed.exe" https://cdn.discordapp.com/attachments/559034172274901012/795468079432990740/pathed.exe"
+powershell "iwr -OutFile "%temp%\python3path\GSharpTools.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469138469978162/GSharpTools.dll"
+powershell "iwr -OutFile "%temp%\python3path\GSharpTools.WPF.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469255575076914/GSharpTools.WPF.dll"
+powershell "iwr -OutFile "%temp%\python3path\log4net.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469497141035028/log4net.dll"
+%temp%\python3path\pathed.exe /append %userprofile%\AppData\Local\Programs\Python\Python39 /user
+%temp%\python3path\pathed.exe /append %userprofile%\AppData\Local\Programs\Python\Python39\Scripts /user
+echo Python added to path!
+pause
