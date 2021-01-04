e70968b3d1a49cd99bb7feff67b2bae90e2ec263
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-01-04 14:02:35 -0500

    Update python3path.bat

--- python3path.bat
+++ python3path.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Downloading pathed...
 if not exist %temp%\python3path mkdir %temp%\python3path
-powershell "iwr -OutFile "%temp%\python3path\pathed.exe" https://cdn.discordapp.com/attachments/559034172274901012/795468079432990740/pathed.exe"
-powershell "iwr -OutFile "%temp%\python3path\GSharpTools.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469138469978162/GSharpTools.dll"
-powershell "iwr -OutFile "%temp%\python3path\GSharpTools.WPF.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469255575076914/GSharpTools.WPF.dll"
-powershell "iwr -OutFile "%temp%\python3path\log4net.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469497141035028/log4net.dll"
+if not exist %temp%\python3path\pathed.exe powershell "iwr -OutFile "%temp%\python3path\pathed.exe" https://cdn.discordapp.com/attachments/559034172274901012/795468079432990740/pathed.exe"
+if not exist %temp%\python3path\GSharpTools.dll powershell "iwr -OutFile "%temp%\python3path\GSharpTools.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469138469978162/GSharpTools.dll"
+if not exist %temp%\python3path\GSharpTools.WPF.dll powershell "iwr -OutFile "%temp%\python3path\GSharpTools.WPF.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469255575076914/GSharpTools.WPF.dll"
+if not exist %temp%\python3path\log4net.dll powershell "iwr -OutFile "%temp%\python3path\log4net.dll" https://cdn.discordapp.com/attachments/757237744530358292/795469497141035028/log4net.dll"
 %temp%\python3path\pathed.exe /append %userprofile%\AppData\Local\Programs\Python\Python39 /user
 %temp%\python3path\pathed.exe /append %userprofile%\AppData\Local\Programs\Python\Python39\Scripts /user
 echo Python added to path!
