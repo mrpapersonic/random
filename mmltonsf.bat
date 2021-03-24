fcee327b13480baf239fc9925a26bd0785dd5815
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-03-24 19:57:28 -0400

    Add mmltonsf.bat

---
+++ mmltonsf.bat
@@ -0,0 +1,30 @@
+@echo off
+
+if "%1" == "" (
+echo usage: compile.bat ^<input^>
+exit /b
+)
+
+REM - Compile MML
+del /f effect.h
+ppmckc.exe -i "%1"
+if not exist "effect.h" (
+echo ppmckc ERROR
+exit /b
+)
+
+REM - Assemble
+del /f ppmck.nes
+nesasm -s -raw ppmck.asm
+if not exist "ppmck.nes" (
+echo nesasm ERROR
+del /f "%~n1" effect.h define.inc
+exit /b
+)
+
+REM - Move files and such
+move /y ppmck.nes "%~n1.nsf"
+echo %~n1.nsf written
+
+REM - Cleanup
+del /f "%~n1".h effect.h define.inc
