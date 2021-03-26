3180dd4de27192a383c622e20cc0719725e876db
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-03-26 00:10:57 -0400

    make tenormoment.bat actually functional

--- tenormoment.bat
+++ tenormoment.bat
@@ -1,8 +1,11 @@
-@if [%1] == [] goto usage
-@if not exist "%1" goto usage
-@if not "%2" == "--debug" @echo off
+@echo off
+REM - file checks
+if [%1] == [] echo usage: %0 ^<video^>
+if not exist %1 echo usage: %0 ^<video^>
 setlocal enabledelayedexpansion
 set "time=0"
+
+REM - folder checks
 :question
 if not exist "%~n1" goto start
 set /p "answer=The folder '%~n1' already exists. Would you like to overwrite or merge the contents? [o/m]: "
@@ -13,18 +16,14 @@ if "%answer%" == "o" (
 if "%answer%" == "m" goto start
 goto question
 
-
+REM - 
 :start
 if not exist "%~n1" mkdir "%~n1"
-ffmpeg -i "%1" -y -t 5 -ss %time% -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "%~n1\gif_%time%.gif"
-for /F "tokens=*" %%F in ("%~n1\gif_%time%.gif") do set "size=%~z1"
-echo %~z1 and !size!
+ffmpeg -i %1 -y -t 5 -ss %time% -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "%~n1\gif_%time%.gif"
+for /F "tokens=*" %%F in ("%~n1\gif_%time%.gif") do set "size=%%~zF"
 if "!size!" == "0" (
 	del "%~n1\gif_%time%.gif"
 	exit /B
 )
 set /a "time=%time%+5"
 goto start
-
-:usage
-echo usage: %0 ^<video^>
