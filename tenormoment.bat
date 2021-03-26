1187224d89c4701335541792793dbedd60bae0d0
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-03-26 00:13:11 -0400

    Update tenormoment.bat

--- tenormoment.bat
+++ tenormoment.bat
@@ -1,23 +1,21 @@
 @echo off
-REM - file checks
-if [%1] == [] echo usage: %0 ^<video^>
-if not exist %1 echo usage: %0 ^<video^>
 setlocal enabledelayedexpansion
 set "time=0"
 
-REM - folder checks
-:question
-if not exist "%~n1" goto start
+if [%1] == [] echo usage: %0 ^<video^>
+if not exist %1 echo usage: %0 ^<video^>
+
+:foldercheck
+if not exist "%~n1" goto encode
 set /p "answer=The folder '%~n1' already exists. Would you like to overwrite or merge the contents? [o/m]: "
 if "%answer%" == "o" (
 	del /f /s /q "%~n1"
-	goto start
+	goto encode
 )
-if "%answer%" == "m" goto start
-goto question
+if "%answer%" == "m" goto encode
+goto foldercheck
 
-REM - 
-:start
+:encode
 if not exist "%~n1" mkdir "%~n1"
 ffmpeg -i %1 -y -t 5 -ss %time% -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "%~n1\gif_%time%.gif"
 for /F "tokens=*" %%F in ("%~n1\gif_%time%.gif") do set "size=%%~zF"
@@ -26,4 +24,4 @@ if "!size!" == "0" (
 	exit /B
 )
 set /a "time=%time%+5"
-goto start
+goto encode
